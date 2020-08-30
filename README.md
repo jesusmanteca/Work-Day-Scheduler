d3fecfd543eccd0692aed1941cc3ce64da142924
Jesus Manteca <jesusmanteca@gmail.com>
2020-08-30 10:17:56 -0500

    added README.md formatting

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Work Day Scheduler Starter Code
+
+Work Day Scheduler is a scheduler where you can add tasks for a single day. The color of each time block is set as follows:
+
+- Gray = Past Due
+- Red = Current
+- Green = Future
+
+All saved items will be saved to the local storage so that you can navigate away from the page and still have information saved.
+
+## User Story
+**As an** employee with a busy schedule, **I want** to add important events to a daily planner **so that** I can manage my time effectively.
+
+## Frameworks/Packages
+- jQuery
+- Bootstrap
+
+
+## Screen Recording
+![Screen Recording Gif](assets/images/work_day_scheduler_demo.gif)
--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// How do I win?
-// GIVEN I am using a daily planner to create a schedule
-
-// TIME DISPLAY
-// branch: feature/time-display
-// WHEN I open the planner... THEN the current day is displayed at the top of the calendar
-
 var currentDay = moment().format('dddd, MMMM Do YYYY')
 var currentHour = moment().hours()
 $("#currentDay").replaceWith(currentDay)
@@ -19,138 +12,12 @@ function updateColors(){
         }else if (thisBlock > currentHour){
             $(this).css("background-color", "lightgreen")
         }
-
     })
 }
 updateColors();
 
 // setting intervals for every 15 minutes to refresh the color codes
 var intervalID = window.setInterval(updateColors, 900000);
-
-
-
-
-// var currentTimeIs9 = $(".time-block-9").data("hour").toString()
-// var currentTimeIs10 = $(".time-block-10").data("hour").toString()
-// var currentTimeIs11 = $(".time-block-11").data("hour").toString()
-// var currentTimeIs12 = $(".time-block-12").data("hour").toString()
-// var currentTimeIs1 = $(".time-block-1").data("hour").toString()
-// var currentTimeIs2 = $(".time-block-2").data("hour").toString()
-// var currentTimeIs3 = $(".time-block-3").data("hour").toString()
-// var currentTimeIs4 = $(".time-block-4").data("hour").toString()
-// var currentTimeIs5 = $(".time-block-5").data("hour").toString()
-
-
-// if (currentTimeIs9 == currentHour) {
-//     console.log("it's 9am")
-//     $("#textArea9").css("background-color", "salmon")
-//     $("#textArea10").css("background-color", "palegreen")
-//     $("#textArea11").css("background-color", "palegreen")
-//     $("#textArea12").css("background-color", "palegreen")
-//     $("#textArea1").css("background-color", "palegreen")
-//     $("#textArea2").css("background-color", "palegreen")
-//     $("#textArea3").css("background-color", "palegreen")
-//     $("#textArea4").css("background-color", "palegreen")
-//     $("#textArea5").css("background-color", "palegreen")
-
-// }
-// if (currentTimeIs10 == currentHour) {
-//     console.log("it's 10am")
-//     $("#textArea9").css("background-color", "lightgray")
-//     $("#textArea10").css("background-color", "salmon")
-//     $("#textArea11").css("background-color", "palegreen")
-//     $("#textArea12").css("background-color", "palegreen")
-//     $("#textArea1").css("background-color", "palegreen")
-//     $("#textArea2").css("background-color", "palegreen")
-//     $("#textArea3").css("background-color", "palegreen")
-//     $("#textArea4").css("background-color", "palegreen")
-//     $("#textArea5").css("background-color", "palegreen")
-// }
-// if (currentTimeIs11 == currentHour) {
-//     console.log("it's 11am")
-//     $("#textArea9").css("background-color", "lightgray")
-//     $("#textArea10").css("background-color", "lightgray")
-//     $("#textArea11").css("background-color", "salmon")
-//     $("#textArea12").css("background-color", "palegreen")
-//     $("#textArea1").css("background-color", "palegreen")
-//     $("#textArea2").css("background-color", "palegreen")
-//     $("#textArea3").css("background-color", "palegreen")
-//     $("#textArea4").css("background-color", "palegreen")
-//     $("#textArea5").css("background-color", "palegreen")
-// }
-// if (currentTimeIs12 == currentHour) {
-//     console.log("it's 12pm")
-//     $("#textArea9").css("background-color", "lightgray")
-//     $("#textArea10").css("background-color", "lightgray")
-//     $("#textArea11").css("background-color", "lightgray")
-//     $("#textArea12").css("background-color", "salmon")
-//     $("#textArea1").css("background-color", "palegreen")
-//     $("#textArea2").css("background-color", "palegreen")
-//     $("#textArea3").css("background-color", "palegreen")
-//     $("#textArea4").css("background-color", "palegreen")
-//     $("#textArea5").css("background-color", "palegreen")
-// }
-// if (currentTimeIs1 == currentHour) {
-//     console.log("it's 1pm")
-//     $("#textArea9").css("background-color", "lightgray")
-//     $("#textArea10").css("background-color", "lightgray")
-//     $("#textArea11").css("background-color", "lightgray")
-//     $("#textArea12").css("background-color", "lightgray")
-//     $("#textArea1").css("background-color", "salmon")
-//     $("#textArea2").css("background-color", "palegreen")
-//     $("#textArea3").css("background-color", "palegreen")
-//     $("#textArea4").css("background-color", "palegreen")
-//     $("#textArea5").css("background-color", "palegreen")
-// }
-// if (currentTimeIs2 == currentHour) {
-//     console.log("it's 2pm")
-//     $("#textArea9").css("background-color", "lightgray")
-//     $("#textArea10").css("background-color", "lightgray")
-//     $("#textArea11").css("background-color", "lightgray")
-//     $("#textArea12").css("background-color", "lightgray")
-//     $("#textArea1").css("background-color", "lightgray")
-//     $("#textArea2").css("background-color", "salmon")
-//     $("#textArea3").css("background-color", "palegreen")
-//     $("#textArea4").css("background-color", "palegreen")
-//     $("#textArea5").css("background-color", "palegreen")
-// }
-// if (currentTimeIs3 == currentHour) {
-//     console.log("it's 3pm")
-//     $("#textArea9").css("background-color", "lightgray")
-//     $("#textArea10").css("background-color", "lightgray")
-//     $("#textArea11").css("background-color", "lightgray")
-//     $("#textArea12").css("background-color", "lightgray")
-//     $("#textArea1").css("background-color", "lightgray")
-//     $("#textArea2").css("background-color", "lightgray")
-//     $("#textArea3").css("background-color", "salmon")
-//     $("#textArea4").css("background-color", "palegreen")
-//     $("#textArea5").css("background-color", "palegreen")
-// }
-// if (currentTimeIs4 == currentHour) {
-//     console.log("it's 4pm")
-//     $("#textArea9").css("background-color", "lightgray")
-//     $("#textArea10").css("background-color", "lightgray")
-//     $("#textArea11").css("background-color", "lightgray")
-//     $("#textArea12").css("background-color", "lightgray")
-//     $("#textArea1").css("background-color", "lightgray")
-//     $("#textArea2").css("background-color", "lightgray")
-//     $("#textArea3").css("background-color", "lightgray")
-//     $("#textArea4").css("background-color", "salmon")
-//     $("#textArea5").css("background-color", "palegreen")
-// }
-// if (currentTimeIs5 == currentHour) {
-//     console.log("it's 5pm")
-//     $("#textArea9").css("background-color", "lightgray")
-//     $("#textArea10").css("background-color", "lightgray")
-//     $("#textArea11").css("background-color", "lightgray")
-//     $("#textArea12").css("background-color", "lightgray")
-//     $("#textArea1").css("background-color", "lightgray")
-//     $("#textArea2").css("background-color", "lightgray")
-//     $("#textArea3").css("background-color", "lightgray")
-//     $("#textArea4").css("background-color", "lightgray")
-//     $("#textArea5").css("background-color", "salmon")
-// }
-
 
 var getSchedule = function(){
 
@@ -181,6 +48,7 @@ var loadSchedule = function(storeData, storeTime){
 
 $(document).ready(function () {
     $(".saveBtn").click(function(){
+
         //get the value from the input box and time and store it as a variable
         var storeData = $(this).siblings("textarea").val().trim();
         var storeTime = $(this).siblings("div").text().trim();
@@ -189,8 +57,7 @@ $(document).ready(function () {
 
         loadSchedule(storeData, storeTime)
 
-        // Save the to-dos into localStorage
-        // We need to use JSON.stringify to turn the list from an array into a string
+        
 
 
     })
